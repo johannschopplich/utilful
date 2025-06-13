78b6286c1e230320d90f9e4c2020957589a790cb
Johann Schopplich <mail@johannschopplich.com>
2025-06-13 13:57:16 +0200

    chore: dynamically infer entry exports from package.json

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,23 +1,31 @@
 import type { UserConfig, UserConfigFn } from 'tsdown/config'
 import { defineConfig } from 'tsdown/config'
 
+const { exports } = await import('./package.json', { with: { type: 'json' } })
+
 const config: UserConfig | UserConfigFn = defineConfig({
-  entry: [
-    'src/index.ts',
-    'src/array.ts',
-    'src/csv.ts',
-    'src/defu.ts',
-    'src/emitter.ts',
-    'src/json.ts',
-    'src/module.ts',
-    'src/object.ts',
-    'src/path.ts',
-    'src/result.ts',
-    'src/string.ts',
-    'src/types.ts',
-  ],
+  entry: [...inferExports(exports)],
   dts: true,
   unbundle: true,
 })
 
 export default config
+
+function inferExports(exports: Record<string, unknown>): Set<string> {
+  const entries = new Set<string>()
+
+  for (const value of Object.values(exports)) {
+    if (typeof value === 'string') {
+      if (value.endsWith('.js')) {
+        entries.add(value.replace('./dist', './src').replace('.js', '.ts'))
+      }
+    }
+    else if (typeof value === 'object' && value !== null) {
+      for (const entry of inferExports(value as Record<string, unknown>)) {
+        entries.add(entry)
+      }
+    }
+  }
+
+  return entries
+}
