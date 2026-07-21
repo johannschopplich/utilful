756516611bfb895082c053b485e6caff3d2ab754
Johann Schopplich <johann@schopplich.com>
2026-07-21 16:32:14 +0200

    feat(json)!: remove cloneJSON in favor of structuredClone

--- src/json.ts
+++ src/json.ts
@@ -20,32 +20,3 @@ export function tryParseJSON<T = unknown>(value: unknown): T {
 }
 
 // #endregion
-
-// #region Clone JSON
-
-/**
- * Clones the given JSON value.
- *
- * @remarks
- * The value must not contain circular references as JSON does not support them. It also must contain JSON serializable values.
- */
-export function cloneJSON<T>(value: T): T {
-  if (typeof value !== 'object' || value === null) {
-    return value
-  }
-
-  if (Array.isArray(value)) {
-    return value.map(element => (typeof element !== 'object' || element === null ? element : cloneJSON(element))) as T
-  }
-
-  const result: Record<string, unknown> = {}
-
-  for (const key in value) {
-    const propertyValue = value[key]
-    result[key] = typeof propertyValue !== 'object' || propertyValue === null ? propertyValue : cloneJSON(propertyValue)
-  }
-
-  return result as T
-}
-
-// #endregion
