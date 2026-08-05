7c8396ad2b1fb42e9932758bb6162406251077e1
Johann Schopplich <johann@schopplich.com>
2026-08-05 16:17:14 +0200

    refactor(types): drop the string autocomplete helpers

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,3 @@
-// #region String types
-
-export type AutocompletableString = string & {}
-
-export type LooseAutocomplete<T extends string> = T | AutocompletableString
-
-// #endregion
-
 // #region Utility types
 
 /** Also commonly referred to as `Prettify`. */
